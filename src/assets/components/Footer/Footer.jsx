b479ce98c6e457e01d019d9bc501cef2d57b9448
Rasmus <97313822+devmus@users.noreply.github.com>
2024-03-01 14:00:06 +0100

    Delete src directory

--- src/assets/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer>
-      <div className="copyright">Copyright &copy; {new Date().getFullYear()} devmus</div>
-    </footer>
-  )
-}
