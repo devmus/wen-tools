96f9e58ecfbe2b45133310f28498d0f587c46745
Rasmus <rasmus.wersall@gmail.com>
2024-03-22 17:48:40 +0100

    :bricks: Update repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Footer } from "./assets/components/Footer/Footer"
 import { Header } from "./assets/components/Header/Header"
 import { MainContentContainer } from "./assets/components/MainContentContainer/MainContentContainer"
-import { Menu } from "./assets/components/Menu/Menu"
+import { Menu } from "./assets/components/Menu/Menu";
 
 function App() {
 
